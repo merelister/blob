1ac17d5cf95f36d6c4d4478a0e11ec68d0d04370
meredith <listermeredith@gmail.com>
2020-03-07 15:40:15 -0800

    writeData method

--- app/src/main/java/com/example/blob/MainActivity.java
+++ app/src/main/java/com/example/blob/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.blob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    double savings;
+    double spendings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test code test
+
+    }
+
+    public void writeData(View v) {
+        String fileContents = savings + "," + spendings;
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput("studentdata.txt", Context.MODE_APPEND);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 //testing working with a different branch
