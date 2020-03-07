b4ac2ce5f3ecd451fc41c1e047a0164a4b93c789
meredith <listermeredith@gmail.com>
2020-03-07 15:11:07 -0800

    saveData method

--- app/src/main/java/com/example/blob/MainActivity.java
+++ app/src/main/java/com/example/blob/MainActivity.java
@@ -2,15 +2,57 @@ package com.example.blob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+/*this is the home screen when opening the app. it shows the blob character(s) and
+buttons to add money to savings or track spending
+ */
+double saved;
+double spent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int test = 0;
+
+        //TODO: find views for buttons and other elements
+
+        //TODO: read data
+        File file = new File(this.getFilesDir(), "userdata.txt");
+    }
+
+    public void addSaved(View v) { //triggered when you click the +savings button
+
+    }
+    public void addSpent(View v) { //triggered when you click the -spending button
+
+    }
+
+    public void saveData(View v) { //write values to a text file
+        String toWrite = saved + "," + spent;
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput("userdata.txt", Context.MODE_APPEND);
+            outputStream.write(toWrite.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Could not save data", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //TODO: write data
     }
 }
 //testing working with a different branch
